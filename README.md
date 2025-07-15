9ca60b01efc5c2d4e1940d343cbfbc80036bc19d
Anisha-Hasan <anishah128@gmail.com>
2025-07-15 16:54:43 +0530

    Initial setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-blockchain-dicom-protection
-React frontend for uploading and controlling access to medical images stored on Ethereum.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>DICOM Blockchain Upload</h1>
+      <p>Welcome to your first React page!</p>
+    </div>
+  );
+}
+
+export default App;

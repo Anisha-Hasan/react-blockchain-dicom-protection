d2c7b4a561d801036b7b294b205be8583f91a7fc
Anisha-Hasan <anishah128@gmail.com>
2025-07-16 14:00:44 +0530

    Added patient and staff login form

--- src/components/PatientLogin.js
+++ src/components/PatientLogin.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 function PatientLogin() {
   return (
-    <div>
-      <h1>Patient Login Page</h1>
+    <div className="container mt-5">
+      <h2 className="mb-4">Patient Login</h2>
+      <form>
+        <div className="mb-3">
+          <label>Patient ID</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your Patient ID"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
     </div>
   );
 }
--- src/components/StaffLogin.js
+++ src/components/StaffLogin.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 function StaffLogin() {
   return (
-    <div>
-      <h1>Staff Login Page</h1>
+    <div className="container mt-5">
+      <h2 className="mb-4">Staff Login</h2>
+      <form>
+        <div className="mb-3">
+          <label>Staff ID</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your Patient ID"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
     </div>
   );
 }

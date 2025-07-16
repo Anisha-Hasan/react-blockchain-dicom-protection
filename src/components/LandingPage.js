1f6d094a14c9422c1d54943caa23528d0e653f64
Anisha-Hasan <anishah128@gmail.com>
2025-07-16 13:50:39 +0530

    Alinged buttons on landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
-    <div className="container">
-      <h1>Welcome to Blockchain DICOM Protection</h1>
-      <Link to="/patient-login">
-        <button>Login as Patient</button>
-      </Link>
-      <Link to="/staff-login">
-        <button>Login as Staff</button>
-      </Link>
+    <div className="container text-center mt-5">
+      <h1 className="mb-4">Blockchain DICOM Protection System</h1>
+      <div className="d-flex justify-content-center gap-3">
+        <Link to="/patient-login">
+          <button className="btn btn-primary btn-lg">Login as Patient</button>
+        </Link>
+        <Link to="/staff-login">
+          <button className="btn btn-success btn-lg">Login as Staff</button>
+        </Link>
+      </div>
     </div>
   );
 }
